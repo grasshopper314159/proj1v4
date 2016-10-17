98ef68d1b1f53ab21b7a98addbabde90abdca769
nclanners <nclanners@gmail.com>
2016-10-16 19:11:39 -0500

    Latest

--- src/proj1v4/UserInterface.java
+++ src/proj1v4/UserInterface.java
@@ -1,3 +1,4 @@
+//Test
 package src.proj1v4;
 /**
  *
@@ -19,7 +20,6 @@ package src.proj1v4;
  * and are not responsible for any loss or damage resulting from its use.
  */
 
- 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -283,12 +283,12 @@ public class UserInterface {
 
 		// Book book = (Book) (issuedBooks.next());
 
-		result = library.renewBook(book.getId(), memberID);
-		if (result != null) {
-			System.out.println(result.getTitle() + "   " + result.getDueDate());
-		} else {
-			System.out.println("Book is not renewable");
-		}
+		// result = library.renewBook(book.getId(), memberID);
+		// if (result != null) {
+		// System.out.println(result.getTitle() + " " + result.getDueDate());
+		// } else {
+		// System.out.println("Book is not renewable");
+		// }
 
 		// Iterator issuedBooks = library.getBooks(memberID);
 
